25f94e4658c9fb485ba6f250c89294deeb9e1d4e
mende273 <mende_273@hotmail.com>
2024-09-29 00:03:00 +0200

    update android version to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "mende273.expandablefilters"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "mende273.expandablefilters"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
